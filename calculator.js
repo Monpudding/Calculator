62b16180fb436d215332f4cdbdb88a508fefa59b
Monpudding <richard.quinilla@gmail.com>
2022-05-21 18:49:36 -0400

    Add the functions for Calculator class, select each document type

--- calculator.js
+++ calculator.js
@@ -9,3 +9,43 @@ const divide = function(a, b) { return a / b};
 const operator = function(operation, a, b){
     return(operation(a,b));
 }
+
+const numberButtons = document.querySelectorAll('operand');
+const operationButtons = document.querySelectorAll('operator');
+const equalsButton = document.querySelector('equals');
+const deleteButton = document.querySelector('delete');
+const allClearButton = document.querySelector('clear');
+const previousOperandEle = document.querySelector('previous-operand');
+const currentOperandEle = document.querySelector('current-operand');
+
+class Calculator {
+    constructor(previousOperandEle, currentOperandEle){
+        this.previousOperandEle = previousOperandEle;
+        this.currentOperandEle = currentOperandEle;
+        this.clear()    
+    }
+}
+
+function clear(){
+
+}
+
+function delete(){
+
+}
+
+function appendNumber(number){
+
+}
+
+function chooseOperation(operation){
+
+}
+
+function compute(){
+
+}
+
+function updateDisplay(){
+
+}
